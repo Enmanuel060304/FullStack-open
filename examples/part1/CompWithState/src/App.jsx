643a37695cbd297f5a4252162d38afe606181aec
Enmanuel Urbina <enmanuelu100@gmail.com>
2025-05-19 22:14:08 -0600

    no me jodas

--- examples/part1/CompWithState/src/App.jsx
+++ examples/part1/CompWithState/src/App.jsx
@@ -1,40 +1,20 @@
-import { useState } from "react";
-import "./App.css";
+// import { useState } from "react";
+import './App.css'
 
-function App() {
-  // const [counter, setCounter] = useState(0);
-  const [isActive, setisActive] = useState(false);
-  // const handleClick = () => setCounter(counter + 1);
-  const handleActive = () => setisActive(!isActive);
-
-  const Display = ({ children, isActive , onClick}) => {
-    return(
-      <>
-        {
-          isActive ? (
-            <div>
-              <p>{children} </p>
-              <button onClick={onClick}>desactivar</button>
-            </div>
-          )
-          :
-          (
-            <p>please active</p>
-          )
-        }
-      </>
-    )
+const App = () => {
+  const persona = {
+    nombre: 'jose',
+    edad: 25
   }
-
+  
+  
+  const newPerson = {...persona, nombre:'Enmanuel', edad: 26}
+  
+  console.log(newPerson);
+  console.log(persona);
   return (
-    <>
-      <div>
-        <button onClick={handleActive}>click me to active</button>
-        <Display isActive={isActive} onClick={handleActive}>hola mundo</Display>
-        {/* <button onClick={handleClick}>click me to plus 1</button> */}
-      </div>
-    </>
-  );
+    <h1>hola mundo</h1>
+  )
 }
 
 export default App;
